cfc78eafe6756dd6ddbdb2192c7dce9d4e0887fb
Chris Devereux <devereux.chris@gmail.com>
2012-12-13 16:29:01 +0000

    OSX demo app

--- Application-OSX/SLSAppDelegate.m
+++ Application-OSX/SLSAppDelegate.m
@@ -7,12 +7,31 @@
 //
 
 #import "SLSAppDelegate.h"
+#import "SLSMarkupParser.h"
+
+@interface SLSAppDelegate () <NSTextViewDelegate>
+@property (unsafe_unretained, nonatomic) IBOutlet NSTextView *displayView;
+@property (unsafe_unretained, nonatomic) IBOutlet NSTextView *markupView;
+@property (unsafe_unretained, nonatomic) IBOutlet NSView *errorMarker;
+@end
+
 
 @implementation SLSAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+}
+
+- (void)textDidChange:(NSNotification *)notification
+{
+    NSAttributedString *str = [SLSMarkupParser stringByParsingTaggedString:self.markupView.string error:NULL];
+    
+    if (str) {
+        [self.displayView.textStorage setAttributedString:str];
+    }
+    
+    [self.errorMarker setHidden:str == nil];
 }
 
 @end
