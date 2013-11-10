3ea5558668937e41e58e128e10892b8f4711d421
Chris Devereux <devereux.chris@gmail.com>
2013-11-10 23:13:08 +0000

    Safe font lookup in +[SLSMarkupParser defaultStyle] using CTFontDescriptor on iOS 7

--- Slash/SLSMarkupParser.m
+++ Slash/SLSMarkupParser.m
@@ -11,17 +11,20 @@
 #import "SLSTaggedRange.h"
 #import "SLSErrors.h"
 
+#import <CoreText/CoreText.h>
 #import <dlfcn.h>
 
 #if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
 #define SUPPORTS_STANDARD_ATTRIBUTES (dlsym(RTLD_DEFAULT, "NSFontAttributeName") != NULL)
+#define BRIDGED_CT_FONT (NSClassFromString(@"UIFontDescriptor") != NULL)
 #define FONT_CLASS UIFont
 #define COLOR_CLASS UIColor
 
 #else
 #import <AppKit/AppKit.h>
 #define SUPPORTS_STANDARD_ATTRIBUTES YES
+#define BRIDGED_CT_FONT YES
 #define FONT_CLASS NSFont
 #define COLOR_CLASS NSColor
 
@@ -29,6 +32,29 @@
 
 #define SetError(ptr, val) if (ptr){ *(ptr) = (val); }
 
+static FONT_CLASS *
+LookupFont(CGFloat pointSize, NSString *fontName, NSString *familyName, CTFontSymbolicTraits symbolicTraits)
+{
+    // https://github.com/chrisdevereux/Slash/issues/8
+    // Font lookup via font name is unsafe on iOS versions > 7.
+    // UIFont lookup via font descriptor is availible on iOS versions < 7.
+    //
+    // In order to safely lookup default fonts, use CTFontDescriptor where CTFont is bridged with UIFont,
+    // otherwise fall back to lookup via font name.
+    
+    if (BRIDGED_CT_FONT) {
+        NSDictionary *traits = @{(__bridge id)kCTFontSymbolicTrait: @(symbolicTraits)};
+        NSDictionary *attributes = @{(__bridge id)kCTFontFamilyNameAttribute: familyName, (__bridge id)kCTFontTraitsAttribute: traits};
+        
+        CTFontDescriptorRef descriptor = CTFontDescriptorCreateWithAttributes((__bridge void *)attributes);
+        FONT_CLASS *font = CFBridgingRelease(CTFontCreateWithFontDescriptor(descriptor, pointSize, NULL));
+        CFRelease(descriptor);
+        
+        return font;
+    } else {
+        return [FONT_CLASS fontWithName:fontName size:pointSize];
+    }
+}
 
 
 @implementation SLSMarkupParser {
@@ -40,7 +66,7 @@
 {
     if (SUPPORTS_STANDARD_ATTRIBUTES) {
         return @{
-            NSFontAttributeName             : [FONT_CLASS fontWithName:@"HelveticaNeue" size:14],
+            NSFontAttributeName             : LookupFont(14, @"HelveticaNeue", @"Helvetica Neue", 0),
             NSForegroundColorAttributeName  : [COLOR_CLASS blackColor],
             NSKernAttributeName             : @0,
             NSParagraphStyleAttributeName   : [NSParagraphStyle defaultParagraphStyle],
@@ -66,15 +92,15 @@
         
         style = @{
             @"$default" : [self defaultAttributes],
-            @"strong"   : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Bold" size:14]},
-            @"em"       : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Italic" size:14]},
-            @"emph"     : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Italic" size:14]},
-            @"h1"       : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Medium" size:48]},
-            @"h2"       : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Medium" size:36]},
-            @"h3"       : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Medium" size:32]},
-            @"h4"       : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Medium" size:24]},
-            @"h5"       : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Medium" size:18]},
-            @"h6"       : @{NSFontAttributeName  : [FONT_CLASS fontWithName:@"HelveticaNeue-Medium" size:16]}
+            @"strong"   : @{NSFontAttributeName  : LookupFont(14, @"HelveticaNeue-Bold", @"Helvetica Neue", kCTFontTraitBold)},
+            @"em"       : @{NSFontAttributeName  : LookupFont(14, @"HelveticaNeue-Italic", @"Helvetica Neue", kCTFontTraitItalic)},
+            @"emph"     : @{NSFontAttributeName  : LookupFont(14, @"HelveticaNeue-Italic", @"Helvetica Neue", kCTFontTraitItalic)},
+            @"h1"       : @{NSFontAttributeName  : LookupFont(48, @"HelveticaNeue-Italic", @"Helvetica Neue", 0)},
+            @"h2"       : @{NSFontAttributeName  : LookupFont(36, @"HelveticaNeue-Italic", @"Helvetica Neue", 0)},
+            @"h3"       : @{NSFontAttributeName  : LookupFont(32, @"HelveticaNeue-Italic", @"Helvetica Neue", 0)},
+            @"h4"       : @{NSFontAttributeName  : LookupFont(24, @"HelveticaNeue-Italic", @"Helvetica Neue", 0)},
+            @"h5"       : @{NSFontAttributeName  : LookupFont(18, @"HelveticaNeue-Italic", @"Helvetica Neue", 0)},
+            @"h6"       : @{NSFontAttributeName  : LookupFont(16, @"HelveticaNeue-Italic", @"Helvetica Neue", 0)}
         };
     });
     
